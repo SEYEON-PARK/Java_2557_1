a70b1585fc560e493fc0049346bfdd35ad1f7065
SEYEON-PARK <62700321+SEYEON-PARK@users.noreply.github.com>
2021-08-11 12:32:47 +0900

    Java언어 BAEKJOON 2557번 문제 풀기!(1)

--- Java_2557_1.java
+++ Java_2557_1.java
@@ -7,7 +7,7 @@ Hello World!를 출력하시오.
 */
 
 class Main{ // Main 클래스
-    public static void main(String [] args){
+    public static void main(String [] args){ // main 메소드
         System.out.println("Hello World!");
     } 
 }
